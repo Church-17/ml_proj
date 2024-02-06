22bed9a80283f87bbb11da0660bd1d73371f76ed
Church-17 <matteo.chiesa.mc@gmail.com>
2024-02-06 13:20:22 +0100

    window2

--- Data_Analisys.py
+++ Data_Analisys.py
@@ -5,13 +5,16 @@ from tkinter.ttk import *
 
 def data_analisys(self):
         self.window2 = tk.Toplevel()
-        self.window2.geometry('280x210')
+        self.window2.geometry('300x230')
         self.title:str = "Info dataset"
         self.window2.title(self.title)
+        self.window2.columnconfigure(0, weight=1)
+        self.window2.rowconfigure(0, weight=1)
 
         self.data_frame = Frame(self.window2)
-        self.data_frame.columnconfigure(0, weight=1)
-        self.data_frame.grid(row=0, column=0, sticky=tk.EW)
+        self.data_frame.columnconfigure((0, 1), weight=1)
+        # self.data_frame.rowconfigure(tuple(range(10)), weight=1)
+        self.data_frame.grid(row=0, column=0, pady=(0, 5))
 
         df =self.dataset
         X=df.iloc[:,:-1] 
@@ -27,14 +30,22 @@ def data_analisys(self):
         max_std = X.std().max()
         mean_std = X.std().mean()
 
-        Label(self.data_frame, text="Data Analisys:", font=("Helvetica", 12)).grid(row=0, column=0)
-        Label(self.data_frame, text=f'Percentuale di bilanciamento: {round(balance_perc,5)}%', font=("Helvetica", 10)).grid(row=1, column=0, padx=4)
-        Label(self.data_frame, text=f'Numero di valori nulli: {round(null_values,5)}', font=("Helvetica", 10)).grid(row=2, column=0, padx=4)
-        Label(self.data_frame, text=f'Numero di valori non nulli: {round(non_null_values,5)}', font=("Helvetica", 10)).grid(row=3, column=0, padx=4)
-        Label(self.data_frame, text=f'Percentuale di valori nulli: {round(null_perc,5)}%', font=("Helvetica", 10)).grid(row=4, column=0, padx=4)
-        Label(self.data_frame, text=f'Numero di outlier: {round(num_outliers,5)}', font=("Helvetica", 10)).grid(row=5, column=0, padx=4)
-        Label(self.data_frame, text=f'Percentuale di outlier: {round(outlier_perc,5)}%', font=("Helvetica", 10)).grid(row=6, column=0, padx=4)
-        Label(self.data_frame, text=f'Deviazione standard minima: {round(min_std,5)}', font=("Helvetica", 10)).grid(row=7, column=0, padx=4)
-        Label(self.data_frame, text=f'Deviazione standard massima: {round(max_std,5)}', font=("Helvetica", 10)).grid(row=8, column=0, padx=4)
-        Label(self.data_frame, text=f'Deviazione standard media: {round(mean_std,5)}', font=("Helvetica", 10)).grid(row=9, column=0, padx=4)
-
+        Label(self.data_frame, text="Data Analisys:", font=("Helvetica", 12)).grid(row=0, column=0, columnspan=2)
+        Label(self.data_frame, text=f'Percentuale di bilanciamento:', font=("Helvetica", 10)).grid(row=1, column=0, padx=4)
+        Label(self.data_frame, text=f'Numero di valori nulli:', font=("Helvetica", 10)).grid(row=2, column=0, padx=4)
+        Label(self.data_frame, text=f'Numero di valori non nulli:', font=("Helvetica", 10)).grid(row=3, column=0, padx=4)
+        Label(self.data_frame, text=f'Percentuale di valori nulli:', font=("Helvetica", 10)).grid(row=4, column=0, padx=4)
+        Label(self.data_frame, text=f'Numero di outlier:', font=("Helvetica", 10)).grid(row=5, column=0, padx=4)
+        Label(self.data_frame, text=f'Percentuale di outlier:', font=("Helvetica", 10)).grid(row=6, column=0, padx=4)
+        Label(self.data_frame, text=f'Deviazione standard minima:', font=("Helvetica", 10)).grid(row=7, column=0, padx=4)
+        Label(self.data_frame, text=f'Deviazione standard massima:', font=("Helvetica", 10)).grid(row=8, column=0, padx=4)
+        Label(self.data_frame, text=f'Deviazione standard media:', font=("Helvetica", 10)).grid(row=9, column=0, padx=4)
+        Label(self.data_frame, text=f'{round(balance_perc,5)}%', font=("Helvetica", 10)).grid(row=1, column=1, padx=4, sticky=tk.W)
+        Label(self.data_frame, text=f'{round(null_values,5)}', font=("Helvetica", 10)).grid(row=2, column=1, padx=4, sticky=tk.W)
+        Label(self.data_frame, text=f'{round(non_null_values,5)}', font=("Helvetica", 10)).grid(row=3, column=1, padx=4, sticky=tk.W)
+        Label(self.data_frame, text=f'{round(null_perc,5)}%', font=("Helvetica", 10)).grid(row=4, column=1, padx=4, sticky=tk.W)
+        Label(self.data_frame, text=f'{round(num_outliers,5)}', font=("Helvetica", 10)).grid(row=5, column=1, padx=4, sticky=tk.W)
+        Label(self.data_frame, text=f'{round(outlier_perc,5)}%', font=("Helvetica", 10)).grid(row=6, column=1, padx=4, sticky=tk.W)
+        Label(self.data_frame, text=f'{round(min_std,5)}', font=("Helvetica", 10)).grid(row=7, column=1, padx=4, sticky=tk.W)
+        Label(self.data_frame, text=f'{round(max_std,5)}', font=("Helvetica", 10)).grid(row=8, column=1, padx=4, sticky=tk.W)
+        Label(self.data_frame, text=f'{round(mean_std,5)}', font=("Helvetica", 10)).grid(row=9, column=1, padx=4, sticky=tk.W)
