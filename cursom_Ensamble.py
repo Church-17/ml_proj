d57c088f1b758cc8519ce3b8d5bdae56de833997
0negip <nomeutentedigoogl@gmail.com>
2024-02-06 09:11:31 +0100

    custom

--- cursom_Ensamble.py
+++ cursom_Ensamble.py
@@ -2,64 +2,54 @@ import numpy as np
 from classification2 import classify
 
 class Custom_Ensemble:
-    def __init__(self, voting, weights):
-        self.estimators = ['Decision tree', 'K nearest neighbor', 'Support Vector Classifier']
+    def __init__(self, weights, voting):
+        self.estimators = {
+            'Decision tree': 0,
+            'K nearest neighbor': 0,
+            'Support Vector Classifier': 0
+        }        
         self.voting = voting
         self.w = weights
         self.fitted = False
-        
-    def fit(self, x, y):
+        self.wsum = weights.sum()
         self.labels = ['0', '1']
-        classifier_params = {}
-        for estimator in self.estimators:
-            if estimator == 'Decision tree':
-                classifier_params['option'] = 'Gini'
-                classify(estimator, x, y, classifier_params )
-            elif  estimator == 'K nearest neighbor':
-                classifier_params['option'] = 'Uniform'
-                classify(estimator, x, y, classifier_params)
-            elif  estimator == 'Support Vector Classifier':
-                classifier_params['option'] = 'Linear'
-                classify(estimator, x, y,  classifier_params)
-        self.fitted = True
+        
 
-    def predict(self, test_x):
-        proba = []
-        i = 0
-        for estimator in self.estimators:
-            proba.append(estimator.predict_proba(test_x) * self.w[i])
-            i +=1
-        proba = np.array(proba)
-        pred_y = []
-        for i in range(0, len(test_x)):
-            if self.voting == 'hard':
-                # Hard voting
-                votes = [np.argmax(p) for p in proba[:,i,:].T]
-                pred_y.append(np.argmax(np.bincount(votes, weights=self.w)))
-            elif self.voting == 'soft':
-                # Soft voting
-                pred_y.append(majority_voting(self.labels, proba[:,i,:].T, self.voting, self.w))
-        return pred_y
+def fit(self, x, y):
+    classifier_params = {}
+    for key in self.estimators.keys():
+        if key == 'Decision tree':
+            classifier_params['option'] = 'Gini'
+        elif key == 'K nearest neighbor':
+            classifier_params['option'] = 'Uniform'
+        elif key == 'Support Vector Classifier':
+            classifier_params['option'] = 'Polinomial'
+        self.estimators[key] = classify(key, x, y, classifier_params)
+    self.fitted = True
 
-def majority_voting(label, proba, voting, w=None):
-    if voting == "hard":    #Hard voting
-        votes = label[np.argmax(proba, axis=0)]# estrae i voti 
-        if w is None: # peso uniforme
-            unique, counts = np.unique(votes, return_counts=True) #conta i voti
-            index = counts.argmax() # estrae il voto massimo
-        else: # voti pesati
+
+def predict(self, test_x):
+    proba = []
+    for key in self.estimators.keys():
+        if key == 'Support Vector Classifier':
+            proba.append(self.estimators[key].decision_function(test_x))
+        else:    
+            proba.append(self.estimators[key].predict_proba(test_x))
+
+    proba = np.array(proba)
+    pred_y = []
+
+    for i in range(0, len(test_x)):
+        if self.voting == 'hard':
+            votes = [np.argmax(p) for p in proba[:,i,:].T]
             w_votes = []
             for i, v in enumerate(votes):
-                w_votes = w_votes + [v]* w[i]
+                w_votes = w_votes + [v] * self.w[i]
             unique, counts = np.unique(w_votes, return_counts=True)
             index = counts.argmax()
-        return unique[index]
-    else: #soft voting
-        if w is None: # peso uniforme
-            s_prob = np.sum(proba, axis=0)
-            index = s_prob.argmax()
-        else: # voti pesati
-            w_s_prob = np.sum(proba*w, axis=0)
+            pred_y.append(unique[index])
+        elif self.voting == 'soft':
+            w_s_prob = np.sum(proba[:,i,:]*self.w, axis=0)
             index = w_s_prob.argmax()
-        return label[index]
-    
+            pred_y.append(self.labels[index])
+    return pred_y
