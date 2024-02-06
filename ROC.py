641527c16cf3adfdc50c97460b1001468cca3a35
xxFrancesco <cardiafrancesco30@gmail.com>
2024-02-06 07:59:44 +0100

    roc

--- ROC.py
+++ ROC.py
@@ -5,6 +5,7 @@ from classification import compute_performances
 def draw_roc_curve(test_y, pred_prob_y):
     fpr, tpr, _ = roc_curve(test_y, pred_prob_y[:,1], pos_label=1)
     auc = roc_auc_score(test_y, pred_prob_y[:,1])
+    plt.title("ROC Curve")
     plt.plot(fpr, tpr)
     plt.annotate("AUC: %.3f" % auc, (0.8, 0))
     plt.show()
