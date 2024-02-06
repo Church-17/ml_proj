1937e98ff98260b838049fd6d0dddfd11ba6e59a
xxFrancesco <cardiafrancesco30@gmail.com>
2024-02-06 11:34:35 +0100

    roc

--- ROC.py
+++ ROC.py
@@ -2,10 +2,15 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, roc_auc_score
 from classification import compute_performances
 
-def draw_roc_curve(test_y, pred_prob_y):
-    fpr, tpr, _ = roc_curve(test_y, pred_prob_y[:,1], pos_label=1)
-    auc = roc_auc_score(test_y, pred_prob_y[:,1])
-    plt.title("ROC Curve")
-    plt.plot(fpr, tpr)
-    plt.annotate("AUC: %.3f" % auc, (0.8, 0))
-    plt.show()
+class ROC_Curve(object):
+    def __init__(self):
+        self.ann = 'ann'
+
+    def draw_roc_curve(self, test_y, pred_prob_y):
+        fpr, tpr, _ = roc_curve(test_y, pred_prob_y[:,1], pos_label=1)
+        auc = roc_auc_score(test_y, pred_prob_y[:,1])
+        plt.title("ROC Curve")
+        plt.plot(fpr, tpr)
+        plt.annotate(str(self.ann), (0.8, 0.2))
+        self.ann = plt.annotate("AUC: %.3f" % auc, (0.8, 0))
+        plt.show()
