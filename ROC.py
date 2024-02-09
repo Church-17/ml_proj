5b216dbea8dbb7614743d924c1ed34487d75bd71
0negip <nomeutentedigoogl@gmail.com>
2024-02-09 12:57:37 +0100

    zio pero

--- ROC.py
+++ ROC.py
@@ -14,8 +14,12 @@ class ROC_Curve(object):
     def draw_roc_curve(self, test_y, pred_prob_y, classifier_name):
         "Plots ROC curve"
         
+        global random_classifier_added
+
         plt.title("ROC Curve")
-        plt.plot((0, 1), (0, 1), color='black', linestyle='dashed', alpha=0.3, label="Random classifier")
+        if not random_classifier_added:
+            plt.plot((0, 1), (0, 1), color='black', linestyle='dashed', alpha=0.3, label="Random classifier")
+            random_classifier_added = True
         
         fpr, tpr, _ = roc_curve(test_y, pred_prob_y[:,1], pos_label=1)
         plt.plot(fpr, tpr, label=f'{classifier_name}: {round(roc_auc_score(test_y, pred_prob_y[:,1]), 3)}')
@@ -25,4 +29,13 @@ class ROC_Curve(object):
         handles, labels = handles[1::2], labels[1::2]
         
         plt.legend([bkc_rnd]+handles, [bkc_lbl]+labels, loc='lower right') 
+
+        def on_close(event):
+            global random_classifier_added
+            random_classifier_added = False
+
+        canvas = plt.gcf().canvas
+        if isinstance(canvas, FigureCanvasBase):  # check if we're using a GUI backend
+            canvas.mpl_connect('close_event', on_close)
+
         plt.show()
