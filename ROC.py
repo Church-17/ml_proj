397f83f18291cd91ba3638b46dd06e31a38e04d9
Church-17 <matteo.chiesa.mc@gmail.com>
2024-02-09 13:03:10 +0100

    cooked

--- ROC.py
+++ ROC.py
@@ -3,9 +3,6 @@ from sklearn.metrics import roc_curve
 from sklearn.metrics import roc_auc_score
 from classification import *
 from numpy import round
-from matplotlib.backend_bases import FigureCanvasBase
-
-random_classifier_added = False
 
 class ROC_Curve(object):
     def __init__(self):
@@ -14,28 +11,14 @@ class ROC_Curve(object):
     def draw_roc_curve(self, test_y, pred_prob_y, classifier_name):
         "Plots ROC curve"
         
-        global random_classifier_added
-
         plt.title("ROC Curve")
-        if not random_classifier_added:
+        _, labels = plt.gca().get_legend_handles_labels()
+        if len(labels) == 0:
             plt.plot((0, 1), (0, 1), color='black', linestyle='dashed', alpha=0.3, label="Random classifier")
-            random_classifier_added = True
         
         fpr, tpr, _ = roc_curve(test_y, pred_prob_y[:,1], pos_label=1)
         plt.plot(fpr, tpr, label=f'{classifier_name}: {round(roc_auc_score(test_y, pred_prob_y[:,1]), 3)}')
         
         handles, labels = plt.gca().get_legend_handles_labels()
-        bkc_rnd, bkc_lbl = handles[0], labels[0]
-        handles, labels = handles[1::2], labels[1::2]
-        
-        plt.legend([bkc_rnd]+handles, [bkc_lbl]+labels, loc='lower right') 
-
-        def on_close(event):
-            global random_classifier_added
-            random_classifier_added = False
-
-        canvas = plt.gcf().canvas
-        if isinstance(canvas, FigureCanvasBase):  # check if we're using a GUI backend
-            canvas.mpl_connect('close_event', on_close)
-
+        plt.legend(handles, labels, loc='lower right') 
         plt.show()
